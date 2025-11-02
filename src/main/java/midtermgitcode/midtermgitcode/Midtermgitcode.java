373704fe9bb96d62b85d837eaca5bf02b80c0de6
Longstone Spio <spio@sheridancollege.ca>
2025-11-02 14:56:40 -0500

    rev 1 local

---
+++ src/main/java/midtermgitcode/midtermgitcode/Midtermgitcode.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package midtermgitcode.midtermgitcode;
+
+/**
+ *
+ * @author Spiol
+ */
+public class Midtermgitcode {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

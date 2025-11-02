13c9b46d6d113d54a19942d7553288e0e14e170c
Longstone Spio <spio@sheridancollege.ca>
2025-11-02 15:00:52 -0500

    rev 3 local

--- src/main/java/midtermgitcode/midtermgitcode/Midtermgitcode.java
+++ src/main/java/midtermgitcode/midtermgitcode/Midtermgitcode.java
@@ -11,6 +11,6 @@ package midtermgitcode.midtermgitcode;
 public class Midtermgitcode {
 
     public static void main(String[] args) {
-        System.out.println("Midterm Git Test rev2");
+        System.out.println("Midterm Git Test rev3");
     }
 }
